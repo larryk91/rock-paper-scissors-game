4077f7f0e2570c706599d407cdc99058ba3f808d
larryk91 <90164535+larryk91@users.noreply.github.com>
2021-09-22 01:21:55 +0900

    Create game.py

---
+++ game.py
@@ -0,0 +1,40 @@
+# game.py
+import random
+
+print("Rock, Paper, Scissors, Shoot!")
+
+user_choice = input("Choose 'rock', 'paper', or 'scissors':  ")
+
+if user_choice in ["rock","paper","scissors"]: 
+    print("You chose:")
+    print(user_choice)
+else:
+    print("Your choice is invalid. Please Try Again!")
+    exit()
+
+
+options = ["rock", "paper", "scissors"]
+comp_choice = random.choice(options)
+print("The computer chose:", comp_choice)
+
+if user_choice == "rock" and comp_choice == "rock":
+    print("You drew")
+elif user_choice == "rock" and comp_choice == "paper":
+    print("Oh the computer won. Maybe nexttime.")
+elif user_choice == "rock" and comp_choice == "scissors": 
+    print("Congratulation! You won!")
+elif user_choice == "paper" and comp_choice == "paper":
+    print("You drew")
+elif user_choice == "paper" and comp_choice == "scissors":
+    print("Oh the computer won. Maybe nexttime.")
+elif user_choice == "paper" and comp_choice == "rock": 
+    print("Congratulation! You won!")
+elif user_choice == "scissors" and comp_choice == "scissors":
+    print("You drew")
+elif user_choice == "scissors" and comp_choice == "rock":
+    print("Oh the computer won. Maybe nexttime.")
+else: 
+    print("Congratulation! You won!")
+
+print("Thanks for playing! Please play again!")
+
